de6494b43cef1936678cd4809063235191e35e93
Sakib Anjum <sakib@technovicinity.com>
2024-03-29 23:34:23 +0600

    update

--- resources/views/frontend/pages/blogs/details.blade.php
+++ resources/views/frontend/pages/blogs/details.blade.php
@@ -3,37 +3,32 @@
 @section('content')
 
       <!-- Cards Holder -->
-      <section class="cards-holder">
+      <section class="page-blog-details">
         <div class="container">
-          <div class="title">
-            {{-- <h4 class="title__sub">Blog post from us</h4>
-            <h2 class="title__main">fsdgts</h2> --}}
-          </div>
-          <div class="cards leading-cards">
 
+          <div class="blog-details">
+            <figure class="figure">
+              <img src="{{asset($blog->blog_image)}}" alt="{{$blog->blog_name}}" />
+            </figure>
+            <h1 class="title">{{$blog->blog_name}}</h1>
+            <div class="blog-meta">
+                    <span>
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg> John Doe
+                    </span>
+                    <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"/></svg>
 
-
-
-            <div class="card-wrap">
-              {{-- <div class="card-heading">Author of the Month</div> --}}
-              <div class="card">
-                <a href="{{url('blogs/'.$blog->id)}}" class="figure">
-                  <img src="{{asset($blog->blog_image)}}" alt="No Image Available" />
-                </a>
-                <div class="text-wrap">
-                  <a href="{{url('blogs/'.$blog->id)}}" class="name">{{$blog->blog_name}}</a>
-                  <p class="bio"></p>
-                  <p class="dsc">
-                    {!! $blog->blog_short_description !!}
-                  </p>
-                  <p class="dsc">
-                    {!!$blog->blog_full_description!!}
-                  </p>
-                  {{-- <a href="{{url('blog/'.$row->id)}}" class="card-link">Follow</a> --}}
-                </div>
+                    10 November 2024
+                    </span>
               </div>
-            </div>
 
+                  <div class="entry-content">
+
+                    {{ $blog->blog_short_description }}
+
+                    {!! $blog->blog_full_description !!}
+
+                  </div>
 
           </div>
         </div>
