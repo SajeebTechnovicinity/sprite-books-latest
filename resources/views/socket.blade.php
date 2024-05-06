82a17ed8fb851b737311c1026daf03ef348af80b
unknown <sajeebchakraborty.cse2000@gmail.com>
2024-05-06 16:35:16 +0600

    ss

--- resources/views/socket.blade.php
+++ resources/views/socket.blade.php
@@ -7,13 +7,12 @@
 </head>
 <body>
     <h1>Socket Test</h1>
-<iframe width="100%" height="100%" frameborder='none' allow="camera" src="https://widget.changelly.com?from=usd&to=btc&amount=150&address=&fromDefault=usd&toDefault=btc&merchant_id=8BoIJVS9TcyByUOv&payment_id=&v=3&type=no-rev-share&color=5f41ff&headerId=1&logo=hide&buyButtonTextId=1">Can't load widget</iframe>
     <button id="emitNewRequest">Emit New Request</button>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.7.5/socket.io.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
-            const socket = io('http://217.196.48.73:8990', { transports: ['websocket'] });
+            const socket = io('https://tripstar-socket.onrender.com', { transports: ['websocket'] });
 
             // Event listener for socket connection
             socket.on('connect', function () {
